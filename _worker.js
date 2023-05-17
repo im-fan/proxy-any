9c34326c8e68aedad3c9d5bd65ea89afbd0e25a6
Wahaha <wyf3634@163.com>
2023-05-17 15:46:17 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,15 +1,43 @@
+// export default {
+//   async fetch(request, env) {
+//     const url = new URL(request.url);
+//     // proxy domain
+//     //api.openai.com
+//     url.host = "chat-gpt-next-web-eight-rose-66.vercel.app";
+//     // openai is already set all CORS heasders 
+//     return fetch(url, {
+//       headers: request.headers,
+//       method: request.method,
+//       body: request.body,
+//       redirect: 'follow'
+//     });
+//   }
+// }
+
+
+import axios from 'axios';
+
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
     // proxy domain
-    //api.openai.com
     url.host = "chat-gpt-next-web-eight-rose-66.vercel.app";
     // openai is already set all CORS heasders 
-    return fetch(url, {
-      headers: request.headers,
+    const response = await axios({
       method: request.method,
-      body: request.body,
-      redirect: 'follow'
+      url: url.toString(),
+      headers: request.headers,
+      data: request.body,
+      maxRedirects: 0,
+      validateStatus: function (status) {
+        return status >= 200 && status < 300;
+      }
     });
+    return {
+      status: response.status,
+      statusText: response.statusText,
+      headers: response.headers,
+      body: response.data
+    };
   }
 }
