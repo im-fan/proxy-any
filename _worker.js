ca9fe061ab95cf92732bb1076c5fef88f79d7ead
Wahaha <wyf3634@163.com>
2023-07-13 17:07:52 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,16 +1,22 @@
 export default {
   async fetch(request, env) {
-    const url = new URL(request.url);
-    // proxy domain
-    //api.openai.com
-    url.host = "api.openai.com";
-    // openai is already set all CORS heasders 
-    return fetch(url, {
-      headers: request.headers,
-      method: request.method,
-      body: request.body,
-      redirect: 'follow',
-      timeout: 5000
-    });
+    try {
+      const url = new URL(request.url);
+      // proxy domain
+      //api.openai.com
+      url.host = "api.openai.com";
+      // openai is already set all CORS heasders 
+      const response = await fetch(url, {
+        headers: request.headers,
+        method: request.method,
+        body: request.body,
+        redirect: 'follow',
+        timeout: 10000
+      });
+      console.log("response==>", response);
+      return response;
+    } catch (error) {
+      console.error(`proxy error: ${error.message}`);
+    }
   }
 }
