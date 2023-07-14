e3d580ec5c64704c08b4c20716069355bd46d6d4
Wahaha <wyf3634@163.com>
2023-07-14 17:01:50 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,28 +1,29 @@
 export default {
   async fetch(request, env) {
-    let retryCount = 0;
-    let response;
-    while (retryCount < 2) {
-        try {
-            const target = "api.openai.com";
-            const url = new URL(target);
-            // proxy domain
-            //api.openai.com
-            // url.host = "api.openai.com";
-            // openai is already set all CORS heasders 
-            const response = await fetch(url, {
-                headers: request.headers,
-                method: request.method,
-                body: request.body,
-                redirect: 'follow',
-                timeout: 10000
-            });
-            break;
-        } catch (error) {
-            console.error(`proxy error: ${error.message}`);
-            retryCount++;
+        let retryCount = 0;
+        let response;
+        while (retryCount < 2) {
+            try {
+                const target = "api.openai.com";
+                const url = new URL(url);
+                // proxy domain
+                //api.openai.com
+                // url.host = "api.openai.com";
+                // openai is already set all CORS heasders 
+                const response = await fetch(url, {
+                    headers: request.headers,
+                    method: request.method,
+                    body: request.body,
+                    redirect: 'follow',
+                    timeout: 10000
+                });
+                break;
+            } catch (error) {
+                console.error(`proxy error: ${error.message}`);
+                retryCount++;
+            }
         }
-    console.log("response==>", response);
-    return response;
-  }
+        console.log("response==>", response);
+        return response;
+    }
 }
