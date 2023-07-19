6dd96be2ff0a04fed97653011de108846fe542dd
Wahaha <wyf3634@163.com>
2023-07-19 20:34:04 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,25 +1,29 @@
-const TELEGRAPH_URL = 'https://api.openai.com';
-
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-
-async function handleRequest(request) {
-  const url = new URL(request.url);
-  url.host = TELEGRAPH_URL.replace(/^https?:\/\//, '');
-
-  const modifiedRequest = new Request(url.toString(), {
-    headers: request.headers,
-    method: request.method,
-    body: request.body,
-    redirect: 'follow'
-  });
-
-  const response = await fetch(modifiedRequest);
-  const modifiedResponse = new Response(response.body, response);
-
-  // 添加允许跨域访问的响应头
-  modifiedResponse.headers.set('Access-Control-Allow-Origin', '*');
-
-  return modifiedResponse;
+export default {
+    async fetch(request, env) {
+        let retryCount = 0;
+        let response;
+        while (retryCount < 2) {
+            try {
+                const target = "api.openai.com";
+                const url = new URL(url);
+                // proxy domain
+                //api.openai.com
+                // url.host = "api.openai.com";
+                // openai is already set all CORS heasders 
+                const response = await fetch(url, {
+                    headers: request.headers,
+                    method: request.method,
+                    body: request.body,
+                    redirect: 'follow',
+                    timeout: 10000
+                });
+                break;
+            } catch (error) {
+                console.error(`proxy error: ${error.message}`);
+                retryCount++;
+            }
+        }
+        console.log("response==>", response);
+        return response;
+    }
 }
